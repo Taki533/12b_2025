8c5ef8e793939443596a602aa3e6a45d2dd4c47c
Taki <Tanulo@DESKTOP-6T5R52A>
2025-09-23 11:25:49 +0200

    pez

---
+++ 12b_2025/pez.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12b_2025
+{
+    internal class pez
+    {
+        public string szin;
+        public string iz;
+        public string csomagolas;
+        public string csomagolasDizajn;
+        public int meret;
+        public int darabszam;
+        public pez(string szin, string iz, string csomagolas, string csomagolasDizajn, int meret, int darbszam)
+        {
+            this.szin = szin;
+            this.iz = iz;
+            this.csomagolas = csomagolas;
+            this.csomagolasDizajn = csomagolasDizajn;
+            this.meret = meret;
+            this.darabszam = darbszam;
+        }
+        public void Megmergez()
+        {
+            Random rand = new Random();
+            int mergez = (int)Math.Floor(rand.NextDouble());
+            if (mergez == 0)
+            {
+                Console.WriteLine("mergzo volt a pez meghaltál!");
+            }
+            else
+            {
+                Console.WriteLine("NEm volt mergezo a pez tul elted!");
+            }
+        }
+    }
+}
