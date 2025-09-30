2b1f8b57cfa9ef9cd132ac00da548e4a5a4093a0
Taki <takacs.bence.22@F4-50>
2025-09-30 11:26:10 +0200

    dolgok_09_30

---
+++ interfesz/AlmaFa.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfesz
+{
+    internal class AlmaFa:fa
+    {
+        public string Név {  get; set; }
+        public void kivag()
+        {
+            Console.WriteLine("Timbeeerrrrrr!!");
+        }
+        public string terem()
+        {
+            return "gyümölcsözik";
+        }
+    }
+}
+    
---
+++ interfesz/fa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfesz
+{
+    internal interface fa
+    {
+        string Név { get; }
+        void kivag();
+        string terem();
+    }
+}
