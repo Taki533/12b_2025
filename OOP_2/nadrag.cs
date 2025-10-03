db96c69dc260cc5ffb4fa3fe0d7f73bfb2de8794
Taki <Tanulo@DESKTOP-6T5R52A>
2025-10-03 09:22:24 +0200

    mostanig

---
+++ OOP_2/nadrag.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_2
+{
+    abstract internal class nadrag:IrUHA
+    {
+       
+        string tipus { get; }
+    }
+}
