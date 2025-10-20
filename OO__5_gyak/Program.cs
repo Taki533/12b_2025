07c82917367af022972067d0243ff1b76ad819e1
Taki <takacs.bence.22@F4-50>
2025-10-20 09:55:25 +0200

    blabla

---
+++ OO__5_gyak/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace OO__5_gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            monitor m1 = new monitor("VGA");
+            monitor m2 = new monitor("VGA", 24);
+        }
+    }
+}
---
+++ OO__5_gyak/monitor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OO__5_gyak
+{
+    internal class monitor
+    {
+        public string felbontas;
+        public double atlo;
+        public monitor(string felbontas)
+        {
+            this.felbontas = felbontas;
+        }
+        public monitor(string felbontas,double kepernyoAtlo)
+        {
+            this.felbontas= felbontas;
+            atlo = kepernyoAtlo;
+        }
+    }
+}
