37c5cd470a99f300a88d08c8d5e21bb72fffec44
Taki <takacs.bence.22@F4-50>
2026-01-05 10:35:44 +0100

    LatinTancok

---
+++ LatinTancok/Adatok.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatinTancok
+{
+    internal class Adatok
+    {
+        public string tancTipus;
+        public string lany;
+        public string fiu;
+        public Adatok(string tanc, string tancos1, string tancos2)
+        {
+            tancTipus = tanc;
+            lany = tancos1;
+            fiu = tancos2;
+        }
+    }
+}
---
+++ LatinTancok/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace LatinTancok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rawData = File.ReadAllLines("tancrend.txt").Chunk(3).ToArray();
+            //Console.WriteLine(rawData);
+            List<Adatok> adatok = new List<Adatok>();
+            for (int i = 0; i < rawData.Length; i++) 
+            {
+                adatok.Add(new Adatok(rawData[i][0], rawData[i][1], rawData[i][2]));
+            }
+            Console.WriteLine("2.feladat");
+            Console.WriteLine($"Az első tánc:{adatok[0].tancTipus} Az utolsó tánc {adatok[adatok.Count - 1].tancTipus}");
+
+            var samba = adatok.Where(tanc => tanc.tancTipus == "samba").ToList();
+            //Console.WriteLine(samba);
+            Console.WriteLine("3.feladat");
+            Console.WriteLine("a sambát ennyiszer adták elő: " + samba.Count);
+            Console.WriteLine("4.feladat");
+            var Vilma = adatok.Where(tanc => tanc.lany=="Vilma").ToList();
+            var VilmaTancai = Vilma.Select(tanc => tanc.tancTipus).ToList();
+            Console.WriteLine($"A táncok amiben Vilma szerepelt : {string.Join(',',VilmaTancai)} ");
+            Console.WriteLine("5.feladat");
+            Console.WriteLine("Adjon meg egy táncot: ");
+            string megadott = Console.ReadLine();
+            var vilmaParja = adatok.Where(tanc => tanc.lany == "Vilma" && tanc.tancTipus == megadott).ToList();
+            if (vilmaParja.Count == 0)
+            {
+                Console.WriteLine($"Vilma nem táncolt {megadott}-t.");
+            }
+            else
+            {
+                Console.WriteLine($"A samba bemutatóján Vilma párja {vilmaParja[0].fiu}  volt.");
+            }
+            Console.WriteLine("6.feladat");
+            var lanyok = adatok.DistinctBy(tanc => tanc.lany).ToList().Select(tanc => tanc.lany);
+            var fiuk = adatok.DistinctBy(tanc => tanc.fiu).ToList().Select(tanc =>tanc.fiu);
+            Console.WriteLine(string.Join(",", fiuk));
+        }
+    }
+}
