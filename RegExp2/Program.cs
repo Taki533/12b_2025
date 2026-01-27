b1e45af4be5d15fbc6fbe17e6045167e318ad008
Taki <takacs.bence.22@ady-nagyatad.hu>
2026-01-27 11:25:42 +0100

    RegExp

---
+++ RegExp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+namespace RegExp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = File.ReadAllText("blabla.txt");
+            Console.WriteLine(szoveg);
+            Regex reg = new Regex("^[A-Z]+\b");
+            MatchCollection match = reg.Matches(szoveg);
+            Console.WriteLine(match.Count);
+        }
+    }
+}
