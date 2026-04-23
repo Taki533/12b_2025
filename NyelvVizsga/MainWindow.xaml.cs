122dfc348b38996203b6039e217573f60c9c4cb8
takacs.bence.22 <takacs.bence.22@F4-59>
2026-04-23 11:25:34 +0200

    asd

---
+++ NyelvVizsga/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+namespace NyelvVizsga
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<vizsga> vizsgakSikeres = new List<vizsga>();
+        public MainWindow()
+        {
+            InitializeComponent();
+                
+
+        }
+        void betolt()
+        {
+            string[] sorokSikeres = File.ReadAllLines("sikeres.csv").Skip(1).ToArray();
+
+            foreach (var sor in sorokSikeres)
+            {
+                vizsgakSikeres.Add(new vizsga(sor));
+            }
+        }
+    }
+}
---
+++ NyelvVizsga/vizsgazo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NyelvVizsga
+{
+    internal class vizsga
+    {
+        public string nyelv;
+        public Dictionary<string, int> evek;
+
+        public vizsga(string nyelv, Dictionary<string, int> evek)
+        {
+            this.nyelv = nyelv;
+            this.evek = evek;
+        }
+        public vizsga(string sor)
+        {
+
+            string[] adatok = sor.Split(';');
+            this.nyelv = adatok[0];
+            this.evek = new Dictionary<string, int>();
+
+            for (int i = 1; i < adatok.Length; i++)
+            {
+                evek[(i + 2008).ToString()] = int.Parse(adatok[i]);
+            }
+        }
+
+    }
+}
